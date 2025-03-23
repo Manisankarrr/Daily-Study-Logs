80d8bf68296fa0f1c1389c0dcd7a0df89780a4ed
Manisankarrr <manish.u2416@gmail.com>
2025-03-23 20:31:25 +0530

    String Methods

---
+++ src/Java/String Methods/Main.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+ public class Main {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+
+        // String name = "Manish";
+        // int length = name.length();
+        // char letter = name.charAt(0);
+        // int index = name.indexOf('r');
+        // int lastIndex = name.lastIndexOf('r');
+
+
+        // String password = "Manish"; 
+
+        // name = name.toUpperCase();
+        // name = name.toLowerCase();
+        // System.out.println(length);
+        // System.out.println(letter);
+        // System.out.println(index);
+        // System.out.println(lastIndex);
+        // System.out.println(name.isEmpty());
+
+        // if(name.equalsIgnoreCase(password))
+        // {
+        //     System.out.println("Password is correct");
+        // }
+        // else{
+        //     System.out.println("Password is incorrect");
+        // }
+
+        //string.substring(start, end)
+
+        // System.out.println("Enter your email:  ");
+
+        // String email = sc.nextLine();
+
+        // // String email = "manish2413@gmail.com";
+
+        // // String username = email.substring(0,10);
+        // String username = email.substring(0, email.indexOf('@'));
+        // String domain = email.substring(11);
+
+        // System.out.println(username);
+        // System.out.println(domain);
+
+
+
+        sc.close();
+        }
+
+
+}
