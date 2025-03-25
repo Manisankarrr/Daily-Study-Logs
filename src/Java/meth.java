9e00f2af5d79c7bb25d2f8b583b7de969ea68a5e
Manisankarrr <manish.u2416@gmail.com>
2025-03-25 20:30:29 +0530

    functions

---
+++ src/Java/meth.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class meth {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name  = "manish";
+
+        happy(name);
+        int num = scanner.nextInt();
+        // double res = square(num);
+        System.out.println(square(num));
+        
+    }
+    static void happy(String name){
+        System.out.printf("Happy Birthday to %s\n",name);}
+    static double square(int number){
+            return number * number;
+        }
+    }
+    
+   
+
