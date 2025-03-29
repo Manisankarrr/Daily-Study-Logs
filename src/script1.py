3ed45e98fd75a15a8c572308201420fcc17070b8
Manisankarrr <manish.u2416@gmail.com>
2025-03-29 17:47:00 +0530

    Python Basics Covered

--- src/script1.py
+++
@@ -1,6 +0,0 @@
-
-
-# from script2 import * # Import everything from script2
-
-if __name__ == "__main__":
-    print("This script is being run directly")
--- src/script2.py
+++
@@ -1,9 +0,0 @@
-#Edit config so that it can run without running main.py like import files(math)
-
-# print(dir())
-
-# print(__name__) #__main__  #__name__ is a built-in variable that represents the name of the module or script being executed.
-
-from script1 import * # Import everything from script1
-
-print(__name__)
