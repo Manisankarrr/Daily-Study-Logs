f92fc5982846cb1927c71bdb3b48630d8bd393e6
Manisankarrr <manish.u2416@gmail.com>
2025-04-04 22:58:51 +0530

    string manipulation

--- src/C/str24.c
+++ src/C/str24.c
@@ -8,24 +8,26 @@ int main()
     printf("Enter a string: ");
     fgets(s, 100, stdin);
     s[strcspn(s, "\n")] = '\0';
+
+    int i, j;
+    char temp[100];
+    fgets(temp, 100, stdin);
+    temp[strcspn(temp, "\n")] = '\0';
     int n = strlen(s),k=0;
-    char reverse[100];
+    char reverse[100],res[100];
 
     char *token  = strtok(s, " ");
     while(token != NULL)
     {
-        int len = strlen(token);
-        int i;
-        for(i = 0; i < len; i++)
+        if (strcmp(token, temp)!=0)
         {
-            reverse[k++] = token[len - i - 1];
+            strcat(res, token);
+            strcat(res, " ");
         }
-        reverse[k++] = ' ';
 
         token = strtok(NULL, " ");        
     }
-    reverse[k-1] = '\0';
-    printf("Reversed string: %s\n", reverse);
+    printf("%s\n", res);
 
     return 0;
     
