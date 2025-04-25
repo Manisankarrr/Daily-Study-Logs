298529fd9db2d1b59d639b4370291a2c890a0548
Manisankarrr <manish.u2416@gmail.com>
2025-04-25 11:33:52 +0530

    bash scripting

---
+++ src/Bash/b1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+#by using above command now can run using ./b1.sh indtead of bash b1.sh
+echo hello world
---
+++ src/Bash/input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo What is your f_name?
+read f_name
+echo What is your l_name?
+read l_name
+
+echo hello $f_name $l_name
