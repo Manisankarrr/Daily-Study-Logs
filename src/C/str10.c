8aea608d40400049cc2bf020f64a59d2a41ca5f1
Manisankarrr <manish.u2416@gmail.com>
2025-05-01 09:09:51 +0530

    C

--- src/C/str10.c
+++ src/C/str10.c
@@ -7,24 +7,39 @@ int main() {
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';  // Remove trailing newline
 
-    char *word[50];
-
+    int k = 0;
+    char *p[50];
     char *token = strtok(s, " ");
-    int count =0;
-    while(token != NULL){
-        word[count++] = token;
+    while(NULL != token)
+    {
+        p[k++] = token;
+        p[k++] = " ";
         token = strtok(NULL, " ");
     }
-    word[count] = '\0';
-    
-    for(int i=count-1;i>=0;i--)
+    p[k-1] = '\0';
+    for(int i=k-1;i>=0;i--)
     {
-        printf("%s",word[i]);
-        if(i!=0)
-        {
-            printf(" ");
-        }
+        if(p[i] != '\0')
+            printf("%s",p[i]);
     }
+    // char *word[50];
+
+    // char *token = strtok(s, " ");
+    // int count =0;
+    // while(token != NULL){
+    //     word[count++] = token;
+    //     token = strtok(NULL, " ");
+    // }
+    // word[count] = '\0';
+    
+    // for(int i=count-1;i>=0;i--)
+    // {
+    //     printf("%s",word[i]);
+    //     if(i!=0)
+    //     {
+    //         printf(" ");
+    //     }
+    // }
 
 
     return 0;
