e2a6cbc2ce206623b5aeaddd3bc5b94a10226a75
Manisankarrr <manish.u2416@gmail.com>
2025-07-30 12:00:47 +0530

    py

--- src/Python/tempCodeRunnerFile.py
+++ src/Python/tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-import json
-
-# file_path = "test/test.json"
-# try:
-#     with open(file_path, 'r') as file:
-#         data = json.load(file)
-#         print(data["employee1"])
-# except FileNotFoundError:
-#     print(f"File not found: {file_path}")
+if pos[c] == 1:
