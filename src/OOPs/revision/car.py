58c1be475985b2ecd9bbdf10c60bad1dfc1190d5
Manisankarrr <manish.u2416@gmail.com>
2025-10-10 18:07:11 +0530

    revising OOPs

---
+++ src/OOPs/revision/car.py
@@ -0,0 +1,11 @@
+# class Car:
+#     #constructor:
+#     def __init__(self, model ):
+#         #attributes:
+#         self.model = model
+
+#     #method:
+#     def drive ( self ):
+#         print(f"driving car {self.model}")
+#     def stop ( self ):
+#         print(f"stopping {self.model}")
---
+++ src/OOPs/revision/main.py
@@ -0,0 +1,34 @@
+
+# # from car import Car
+
+# # #objects:
+# # c1 = Car("BMW")
+# # c2 = Car("Audi")
+
+# # c1.drive()
+# # c1.stop()
+# # print(c1.model)
+# # print(c2.model)
+
+# class Student:
+
+#     class_year = 2024  # class attribute
+#     num_students = 0  # class attribute     
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         Student.num_students += 1  # Increment the count of students
+
+#     def study(self):
+#         print(f"{self.name} is studying.")
+
+#     def take_exam(self):
+#         print(f"{self.name} is taking an exam.")
+
+# s1 = Student("Alice", 20)
+# s2 = Student("Bob", 22)
+
+# s1.study()
+# s2.take_exam()
+# print(s1.class_year)
+# print(Student.num_students)
