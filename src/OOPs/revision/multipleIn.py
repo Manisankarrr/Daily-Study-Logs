5fb74a86858b3308dd138886499824c640999646
Manisankarrr <manish.u2416@gmail.com>
2026-02-07 20:04:57 +0530

    oops

---
+++ src/OOPs/revision/multipleIn.py
@@ -0,0 +1,11 @@
+class Parent1:
+    def method1(self):
+        print("Method from Parent1")
+
+class Parent2:
+    def method2(self):
+        print("Method from Parent2")
+
+class Child(Parent1, Parent2):
+    def method3(self):
+        print("Method from Child")
